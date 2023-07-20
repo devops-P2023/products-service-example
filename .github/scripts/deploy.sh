abf534b3e2e7531ca1bfdf07f2d3eee68289044e
Santiago Langwagen <slangwagen@gmail.com>
2023-07-20 16:05:36 -0300

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -8,10 +8,10 @@ echo "Listing nodes"
 kubectl get nodes
 
 ## Desplegar un Pod de nombre pod-2048 usando la imagen evilroot/docker-2048 y pasándole una etiqueta app=pod-2048
-kubectl run pod-product-service --image=$IMAGE -l app=$APP
+kubectl run $APP --image=$IMAGE -l app=$APP
 
 ## Exponer el pod usando un Service del tipo LoadBalancer. De esta forma, se crea un ALB directamente en AWS.
-kubectl expose pod pod-product-service --port=80  --name=svc-product-service --type=LoadBalancer
+kubectl expose $APP --port=80  --name=svc-product-service --type=LoadBalancer
 
  ## Podemos validar que se haya creado el service y obtener la url del balanceador
  kubectl get svc
